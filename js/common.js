130da333af8c9a6fe72111c2eedf59b8372a097b
unknown <malaivasili@gmail.com>
2019-04-14 18:22:40 +0300

    Сверстал адаптивное меню+ выезжающее меню

---
+++ js/common.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $(".menu-button").on('click', function(){
+    $('.menu-button').toggleClass('menu-button-active'),
+    $('.nav__menu').toggleClass('nav__menu-active')
+  });
+});
