7b2eef9a604dc1c3da5123aca9d0b15cbe415823
unknown <malaivasili@gmail.com>
2019-04-21 14:11:18 +0300

    Верстка блока услуг + частично промо

--- js/common.js
+++ js/common.js
@@ -1,7 +1,17 @@
-$(document).ready(function(){
-  $(".menu-button").on('click', function(){
+$(document).ready(function () {
+  $('.menu-button').on('click', function () {
     $('.menu-button').toggleClass('menu-button-active'),
     $('.nav__menu').toggleClass('nav__menu-active')
   });
 
+  $('.promo__item').on('click', function(){
+    $('.promo__item').removeClass('promo__item-active');
+    $(this).addClass('promo__item-active');
+  });
+  
+    
+
+  $('.header__slider').slick({
+    dots: true
+  });
 });
