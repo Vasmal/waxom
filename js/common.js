662e910d0b2133912a6c824df5a0a7eeef0d88b3
unknown <malaivasili@gmail.com>
2019-04-22 15:22:03 +0300

    Правки в js

--- js/common.js
+++ js/common.js
@@ -14,14 +14,12 @@ $(document).ready(function () {
   
   //promo section tabs
   $('.promo__item').on('click', function(){
-    e.preventDefault();
     $('.promo__item').removeClass('promo__item-active');
     $(this).addClass('promo__item-active');
   });
   
   //gallery mix it up initialization
   $('.gallery__tabs button').on('click', function(){
-    
     $('.gallery__tabs button').removeClass('gallery__tab-active');
     $(this).addClass('gallery__tab-active');
   });
